540eae41b202ac9f84a10e71465574022eba76c2
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-07-23 22:05:25 +0200

    new navigation added. construction of pages refactored.

---
+++ portfolio/wwwroot/interop.js
@@ -0,0 +1,24 @@
+﻿function scrollToElement(element) {
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+
+function scrollToElementByID(ID) {
+    let element = document.getElementById(ID);
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+
+getBodyScrolPosition: () => {
+    return document.body.scrollTop;
+}
+
+getWindowHeight: () => {
+    return window.hei
+}
