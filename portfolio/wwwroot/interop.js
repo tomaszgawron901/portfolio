02bd98419c3a17aab20f68e68572eeb40a4c8787
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-07-24 18:49:26 +0200

    topbar activates properly but some warning araises (GC_MINOR: (Nursery full))

--- portfolio/wwwroot/interop.js
+++ portfolio/wwwroot/interop.js
@@ -15,10 +15,35 @@ function scrollToElementByID(ID) {
     }
 }
 
-getBodyScrolPosition: () => {
-    return document.body.scrollTop;
+function isInView(element) {
+    let parent = element.offsetParent
+    let scrollPosition = parent.clientHeight / 2 + parent.scrollTop;
+
+    if (scrollPosition > element.offsetTop && scrollPosition <= element.offsetTop + element.offsetHeight) {
+        return true;
+    }
+    return false;
 }
 
-getWindowHeight: () => {
-    return window.hei
+function initializeTabControl(ref) {
+    ref.pages = [];
+    ref.onscroll = () => {
+        let outp = -1;
+        let scrollPosition = ref.clientHeight / 2 + ref.scrollTop;
+        for (let i = 0; i < ref.pages.length; i++) {
+            if (scrollPosition > ref.pages[i].offsetTop && scrollPosition <= ref.pages[i].offsetTop + ref.pages[i].offsetHeight) {
+                outp = i;
+                break;
+            }
+        }
+        console.log(outp);
+    }
+    console.log(ref);
+    console.log(ref.pages);
+    console.log(ref.onscroll);
+}
+
+function addPage(ref, index, page) {
+    ref.pages[index] = page;
+    console.log(ref.pages)
 }
