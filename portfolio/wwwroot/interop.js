c549327def14770fc229eec6e64347671edb5934
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-07-25 16:34:37 +0200

    active page is centered after 1s since last scroll.

--- portfolio/wwwroot/interop.js
+++ portfolio/wwwroot/interop.js
@@ -19,35 +19,8 @@ function isInView(element) {
     let parent = element.offsetParent
     let scrollPosition = parent.clientHeight / 2 + parent.scrollTop;
 
-    if (scrollPosition > element.offsetTop && scrollPosition <= element.offsetTop + element.offsetHeight) {
+    if ( scrollPosition <= element.offsetTop + element.offsetHeight && scrollPosition > element.offsetTop) {
         return true;
     }
     return false;
-}
-
-function getActive(ref) {
-    return ref.activePage;
-}
-
-function initializeTabControl(ref, control) {
-    ref.pages = [];
-    ref.activePage = -1;
-    ref.onscroll = () => {
-        let outp = -1;
-        let scrollPosition = ref.clientHeight / 2 + ref.scrollTop;
-        for (let i = 0; i < ref.pages.length; i++) {
-            if (scrollPosition <= ref.pages[i].offsetTop + ref.pages[i].offsetHeight) {
-                outp = i;
-                break;
-            }
-        }
-        if (ref.activePage != outp) {
-            ref.activePage = outp;
-            control.invokeMethodAsync("SetActivePage", ref.activePage);
-        }
-    }
-}
-
-function addPage(ref, index, page) {
-    ref.pages[index] = page;
 }
