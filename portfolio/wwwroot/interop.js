9c231174d00ab1629bd63add2c5b959f9adf5994
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-07-25 22:28:53 +0200

    Properly centers pages longer than 100% of offsetParent height. Page can be higher than 100% of offsetParent height.

--- portfolio/wwwroot/interop.js
+++ portfolio/wwwroot/interop.js
@@ -6,13 +6,30 @@
     }
 }
 
-function scrollToElementByID(ID) {
-    let element = document.getElementById(ID);
-    if (element) {
+// !make sure that element is in view(is active) before using it.
+function centerElement(element) {
+    let parent = element.offsetParent
+    let halfOfParentHeight = parent.clientHeight / 2;
+    let scrollPosition = halfOfParentHeight + parent.scrollTop;
+
+    let topBoundary = element.offsetTop + halfOfParentHeight;
+    if (scrollPosition < topBoundary) {
         element.scrollIntoView({
-            behavior: 'smooth'
+            behavior: 'smooth',
+            block: 'start'
         });
+        return;
     }
+
+    let bottomBoundary = element.offsetTop + element.offsetHeight - halfOfParentHeight;
+    if (scrollPosition > bottomBoundary) {
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'end'
+        });
+        return;
+    }
+
 }
 
 function isInView(element) {
