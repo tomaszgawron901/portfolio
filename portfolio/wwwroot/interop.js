e7e504ad4e9ffab100defa549c61613a6bab2bbf
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-08-11 17:42:04 +0200

    scrolling bugs fixed

--- portfolio/wwwroot/interop.js
+++ portfolio/wwwroot/interop.js
@@ -1,32 +1,45 @@
 ﻿function scrollToElement(element) {
-    if (element) {
-        element.scrollIntoView({
+    parent = element.offsetParent;
+    if (element.scrollTo) {
+        parent.scrollTo({
+            top: element.offsetTop,
+            left: 0,
             behavior: 'smooth'
-        });
+        })
+    } else {
+        parent.scrollTop = element.offsetTop;
     }
 }
 
 // !make sure that element is in view(is active) before using it.
 function centerElement(element) {
-    let parent = element.offsetParent
+    if (element.scrollTo == null) {
+        return;
+    }
+
+    let parent = element.offsetParent;
     let halfOfParentHeight = parent.clientHeight / 2;
     let scrollPosition = halfOfParentHeight + parent.scrollTop;
 
     let topBoundary = element.offsetTop + halfOfParentHeight;
     if (scrollPosition < topBoundary) {
-        element.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+        destination = element.offsetTop;
+        parent.scrollTo({
+            top: destination,
+            left: 0,
+            behavior: 'smooth'
+        })
         return;
     }
 
     let bottomBoundary = element.offsetTop + element.offsetHeight - halfOfParentHeight;
     if (scrollPosition > bottomBoundary) {
-        element.scrollIntoView({
-            behavior: 'smooth',
-            block: 'end'
-        });
+        destination = element.offsetTop + element.offsetHeight - parent.clientHeight;
+        parent.scrollTo({
+            top: destination,
+            left: 0,
+            behavior: 'smooth'
+        })
         return;
     }
 
