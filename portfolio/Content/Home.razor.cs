11101c862d05100f9984824865d36cbbbaa00dec
tomaszgawron901 <mrgawron901@gmail.com>
2022-07-04 20:28:25 +0200

    removing scroll tab control scroll functionality from Page Class to Index page.

--- portfolio/Content/Home.razor.cs
+++ portfolio/Content/Home.razor.cs
@@ -14,16 +14,19 @@ namespace portfolio.Content
 {
     public partial class Home
     {
-        public Home()
-        {
+        [Parameter] public EventCallback OnLearnMoreClicked { get; set; }
 
-        }
+        public Home(){}
         
-
         protected override void OnParametersSet()
         {
             Icon = "oi oi-home";
             Label = "Home";
+        }
+        
+        private Task LearnMoreClicked()
+        {
+            return OnLearnMoreClicked.InvokeAsync();
         }
 
     }
---
+++ portfolio/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using portfolio.Components;
+using System.Threading.Tasks;
+
+namespace portfolio.Pages
+{
+    public partial class Index: ComponentBase
+    {
+        private TabControll? TabControll { get; set; }
+        private Task LearnMoreClicked()
+        {
+            if (TabControll is null) return Task.CompletedTask;
+
+            return TabControll.ScrollTo(1);
+        }
+    }
+}
