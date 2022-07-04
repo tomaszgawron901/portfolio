21df2ff35cb438166084d3b5bb6cd3605aa33ebd
tomaszgawron901 <mrgawron901@gmail.com>
2022-07-04 20:29:49 +0200

    add plus, minus operators to DOMEventBridge.

--- portfolio/JsInteropTolls/PageInteraction/DOMEventBridge.cs
+++ portfolio/JsInteropTolls/PageInteraction/DOMEventBridge.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace portfolio.JsInteropTolls.PageInteraction
 {
-    public class DOMEventBridge<T> where T: class
+    public class DOMEventBridge<T> where T: EventArgs
     {
         public delegate void DOMEventHandler(T args);
         private DOMEventHandler? DotNetEventHandlers;
@@ -23,6 +23,18 @@ namespace portfolio.JsInteropTolls.PageInteraction
         public void JsInvoke(T args)
         {
             DotNetEventHandlers?.Invoke(args);
+        }
+
+        public static DOMEventBridge<T> operator + (DOMEventBridge<T> bridge, DOMEventHandler eventHandler)
+        {
+            bridge.Subscribe(eventHandler);
+            return bridge;
+        }
+
+        public static DOMEventBridge<T> operator - (DOMEventBridge<T> bridge, DOMEventHandler eventHandler)
+        {
+            bridge.Unsubscribe(eventHandler);
+            return bridge;
         }
     }
 }
